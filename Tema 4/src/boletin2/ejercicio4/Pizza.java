ff73e88d334a06dcc7cd7f7f0917cb21bbe28988
Fg-21 <fm.gonzalez@iesnervion.es>
2025-02-12 09:01:58 +0100

    4 DONE

--- Tema 4/src/boletin2/ejercicio4/Pizza.java
+++ Tema 4/src/boletin2/ejercicio4/Pizza.java
@@ -1,29 +1,32 @@
 package boletin2.ejercicio4;
 
 public class Pizza {
-	// Código de cada pizza
-	private int codigo = 000000;
-
-	// Tamaño de cada pizza
-	enum tamaño {
+	// Enumerado del tamaño de cada pizza
+	enum Tamaño {
 		MEDIANA, FAMILIAR
 	}
 
-	private tamaño size = tamaño.MEDIANA;
-
-	// Tipo de cada pizza
-	enum tipo {
+	// Enumerado del tipo de cada pizza
+	enum Tipo {
 		MARGARITA, CUATRO_QUESOS, FUNGHI
 	}
 
-	private tipo type = tipo.MARGARITA;
-
-	// Estado de la pizza
-	enum estado {
+	// Enumerado del estado de la pizza
+	enum Estado {
 		PEDIDA, SERVIDA
 	}
 
-	private estado state = estado.PEDIDA;
+	// Código de cada pizza
+	private int codigo = 000000;
+	
+	// Tipo de la pizza
+	private Tipo type = Tipo.MARGARITA;
+	
+	// Tamaño de la pizza
+	private Tamaño size = Tamaño.MEDIANA;
+	
+	//Estado de la pizza
+	private Estado state = Estado.PEDIDA;
 
 	/**
 	 * Constructor de la Pizza
@@ -32,35 +35,10 @@ public class Pizza {
 	 * @param size   Tamaño de la pizza
 	 * @param type   Tipo/Sabor de la Pizza
 	 */
-	Pizza(int codigo, String size, String type) {
+	public Pizza(int codigo, String size, String type) {
 		this.codigo = codigo;
-
-		switch (size) {
-		case "MEDIANA" -> {
-			this.size = tamaño.MEDIANA;
-		}
-		case "FAMILIAR" -> {
-			this.size = tamaño.FAMILIAR;
-		}
-		default ->{
-			this.size = tamaño.MEDIANA;
-		}
-		}
-		
-		switch (type) {
-		case "MARGARITA"->{
-			this.type = tipo.MARGARITA;
-		}
-		case "CUATRO_QUESOS"->{
-			this.type = tipo.CUATRO_QUESOS;
-		}
-		case "FUNGHI"->{
-			this.type = tipo.FUNGHI;
-		}
-		default ->{
-			this.type = tipo.MARGARITA;
-		}
-		}
+		sizeComp(size.toUpperCase());
+		typeComp(type.toUpperCase());
 	}
 
 	/**
@@ -85,7 +63,7 @@ public class Pizza {
 	 * Getter del tamaño de la pizza
 	 * @return Devuelve el tamaño de la pizza
 	 */
-	public tamaño getSize() {
+	public Tamaño getSize() {
 		return size;
 	}
 
@@ -93,15 +71,15 @@ public class Pizza {
 	 * Setter del tamaño de la pizza
 	 * @param size El tamaño de la pizza. No puede ser null nu estar vacío
 	 */
-	public void setSize(tamaño size) {
-		this.size = size;
+	public void setSize(String size) {
+		sizeComp(size.toUpperCase());
 	}
 	
 	/**
 	 * Getter del tipo de la pizza
 	 * @return Devuelve el tipo de la pizza
 	 */
-	public tipo getType() {
+	public Tipo getType() {
 		return type;
 	}
 
@@ -109,13 +87,105 @@ public class Pizza {
 	 * Setter del tipo de la Pizza
 	 * @param type El tipo de la Pizza
 	 */
-	public void setType(tipo type) {
-		this.type = type;
+	public void setType(String type) {
+		typeComp(type.toUpperCase());
 	}
 
-	public estado getState() {
+	/**
+	 * Getter del estado de la pizza
+	 * @return Devuelve el estado de la pizza
+	 */
+	public Estado getState() {
 		return state;
 	}
 	
+	/**
+	 * Setter del estado de la pizza, no se pueden poner otros estados que no sean PEDIDA o SERVIDA
+	 * @param estado El estado de la pizza
+	 */
+	public void setState(String estado) {
+		stateComp(estado.toUpperCase());
+		
+	}
+	
+	
+	
+	/**
+	 * Metodo para comprobar la asignación correcta del tamaño de la pizza
+	 * @param size El tamaño de la pizza
+	 */
+	private void sizeComp(String size) {
+		switch (size) {
+		case "MEDIANA" -> {
+			this.size = Tamaño.MEDIANA;
+		}
+		case "FAMILIAR" -> {
+			this.size = Tamaño.FAMILIAR;
+		}
+		default ->{
+			this.size = Tamaño.MEDIANA;
+		}
+		}
+	}
+	
+	/**
+	 * Método para comprobar la correcta asignación del tipo de la pizza
+	 * @param type El tipo de la Pizza
+	 */
+	private void typeComp(String type) {
+		switch (type) {
+		case "MARGARITA"->{
+			this.type = Tipo.MARGARITA;
+		}
+		case "CUATRO_QUESOS"->{
+			this.type = Tipo.CUATRO_QUESOS;
+		}
+		case "FUNGHI"->{
+			this.type = Tipo.FUNGHI;
+		}
+		default ->{
+			this.type = Tipo.MARGARITA;
+		}
+		}
+	}
+	
+	/**
+	 * Metodo para comprobar la correcta asignación del estado de la pizza
+	 * @param state El estado de la pizza
+	 */
+	private void stateComp(String state) {
+		switch(state) {
+		case "PEDIDA" ->{
+			this.state = Estado.PEDIDA;
+		}
+		case "SERVIDA" ->{
+			this.state = Estado.SERVIDA;
+		}
+		default ->{
+			this.state = Estado.PEDIDA;
+		}
+		}
+	}
+	
+	/**
+	 * To String que muestra los atributos de la pizza por consola
+	 */
+	public String toString() {
+		return this.codigo + " " + this.size + " - " + this.type + " - " + this.state; 
+	}
+	
+	/**
+	 * Método que sobreescribe el equals de la clase object
+	 * @param Objeto Pizza
+	 */
+	public boolean equals(Object pizza) {
+		boolean iguales = false;
+		Pizza pizzaParam = (Pizza) pizza;
+		
+		if (pizzaParam.codigo == this.codigo) {
+			iguales = true;
+		}
+		return iguales;
+	}
 	
 }
