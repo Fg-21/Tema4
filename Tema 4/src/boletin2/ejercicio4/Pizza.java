29700cf030efba0b1999b5d86a301774c1b6b7b6
Fg-21 <fm.gonzalez@iesnervion.es>
2025-02-17 12:42:52 +0100

    1 && 2 DONE

--- Tema 4/src/boletin2/ejercicio4/Pizza.java
+++ Tema 4/src/boletin2/ejercicio4/Pizza.java
@@ -18,14 +18,14 @@ public class Pizza {
 
 	// Código de cada pizza
 	private int codigo = 000000;
-	
+
 	// Tipo de la pizza
 	private Tipo type = Tipo.MARGARITA;
-	
+
 	// Tamaño de la pizza
 	private Tamaño size = Tamaño.MEDIANA;
-	
-	//Estado de la pizza
+
+	// Estado de la pizza
 	private Estado state = Estado.PEDIDA;
 
 	/**
@@ -43,6 +43,7 @@ public class Pizza {
 
 	/**
 	 * Getter del código de la pizza
+	 * 
 	 * @return Devuelve el código de la pizza
 	 */
 	public int getCodigo() {
@@ -51,6 +52,7 @@ public class Pizza {
 
 	/**
 	 * Setter del Código de la pizza. No se pueden poner negativo
+	 * 
 	 * @param codigo El código de la pizza
 	 */
 	public void setCodigo(int codigo) {
@@ -61,6 +63,7 @@ public class Pizza {
 
 	/**
 	 * Getter del tamaño de la pizza
+	 * 
 	 * @return Devuelve el tamaño de la pizza
 	 */
 	public Tamaño getSize() {
@@ -69,14 +72,16 @@ public class Pizza {
 
 	/**
 	 * Setter del tamaño de la pizza
+	 * 
 	 * @param size El tamaño de la pizza. No puede ser null nu estar vacío
 	 */
 	public void setSize(String size) {
 		sizeComp(size.toUpperCase());
 	}
-	
+
 	/**
 	 * Getter del tipo de la pizza
+	 * 
 	 * @return Devuelve el tipo de la pizza
 	 */
 	public Tipo getType() {
@@ -85,6 +90,7 @@ public class Pizza {
 
 	/**
 	 * Setter del tipo de la Pizza
+	 * 
 	 * @param type El tipo de la Pizza
 	 */
 	public void setType(String type) {
@@ -93,99 +99,77 @@ public class Pizza {
 
 	/**
 	 * Getter del estado de la pizza
+	 * 
 	 * @return Devuelve el estado de la pizza
 	 */
 	public Estado getState() {
 		return state;
 	}
-	
+
 	/**
-	 * Setter del estado de la pizza, no se pueden poner otros estados que no sean PEDIDA o SERVIDA
+	 * Setter del estado de la pizza, no se pueden poner otros estados que no sean
+	 * PEDIDA o SERVIDA
+	 * 
 	 * @param estado El estado de la pizza
 	 */
 	public void setState(String estado) {
 		stateComp(estado.toUpperCase());
-		
+
 	}
-	
-	
-	
+
 	/**
 	 * Metodo para comprobar la asignación correcta del tamaño de la pizza
+	 * 
 	 * @param size El tamaño de la pizza
 	 */
 	private void sizeComp(String size) {
 		switch (size) {
-		case "MEDIANA" -> {
-			this.size = Tamaño.MEDIANA;
-		}
-		case "FAMILIAR" -> {
-			this.size = Tamaño.FAMILIAR;
-		}
-		default ->{
-			this.size = Tamaño.MEDIANA;
-		}
+		case "MEDIANA", "FAMILIAR" -> this.size = Tamaño.valueOf(size);
 		}
 	}
-	
+
 	/**
 	 * Método para comprobar la correcta asignación del tipo de la pizza
+	 * 
 	 * @param type El tipo de la Pizza
 	 */
 	private void typeComp(String type) {
 		switch (type) {
-		case "MARGARITA"->{
-			this.type = Tipo.MARGARITA;
-		}
-		case "CUATRO_QUESOS"->{
-			this.type = Tipo.CUATRO_QUESOS;
-		}
-		case "FUNGHI"->{
-			this.type = Tipo.FUNGHI;
-		}
-		default ->{
-			this.type = Tipo.MARGARITA;
-		}
+		case "MARGARITA", "CUATRO_QUESOS", "FUNGHI" -> this.type = Tipo.valueOf(type);
 		}
 	}
-	
+
 	/**
 	 * Metodo para comprobar la correcta asignación del estado de la pizza
+	 * 
 	 * @param state El estado de la pizza
 	 */
 	private void stateComp(String state) {
-		switch(state) {
-		case "PEDIDA" ->{
-			this.state = Estado.PEDIDA;
-		}
-		case "SERVIDA" ->{
-			this.state = Estado.SERVIDA;
-		}
-		default ->{
-			this.state = Estado.PEDIDA;
-		}
+		switch (state) {
+		case "PEDIDA", "SERVIDA" -> this.state = Estado.valueOf(state);
 		}
 	}
-	
+
 	/**
 	 * To String que muestra los atributos de la pizza por consola
 	 */
 	public String toString() {
-		return this.codigo + " " + this.size + " - " + this.type + " - " + this.state; 
+		return this.codigo + " " + this.size + " - " + this.type + " - " + this.state;
 	}
-	
+
 	/**
 	 * Método que sobreescribe el equals de la clase object
+	 * 
 	 * @param Objeto Pizza
 	 */
 	public boolean equals(Object pizza) {
 		boolean iguales = false;
 		Pizza pizzaParam = (Pizza) pizza;
-		
+
 		if (pizzaParam.codigo == this.codigo) {
 			iguales = true;
 		}
 		return iguales;
 	}
-	
+
 }
